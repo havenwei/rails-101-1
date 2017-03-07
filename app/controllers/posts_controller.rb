213dfff451e39411f15ca50d6681bed39b3471cb
havenwei <havenwei82@163.com>
2017-03-07 13:01:24 +0800

    implement posts#edit/update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notic: 'Update Success'
+    else
+      render :edit  
+    end
+  end
+
   private
 
   def post_params

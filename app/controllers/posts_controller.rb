f9b166d2c59226850596d79b1aed26b640d57436
havenwei <havenwei82@163.com>
2017-03-07 13:34:31 +0800

    check owner permission when access edit/update/destroy,use before_action to find_post_and_check_permission

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+  before_action :find_post_and_check_permission, :only =>[:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -21,11 +22,9 @@ class PostsController < ApplicationController
 
   def edit
     @group = Group.find(params[:group_id])
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to account_posts_path, notic: 'Update Success'
     else
@@ -34,7 +33,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to account_posts_path, alert: 'Post deleted'
   end
@@ -43,6 +41,14 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def find_post_and_check_permission
+    @post = Post.find(params[:id])
+
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no permission."
+    end
   end
 
 end
